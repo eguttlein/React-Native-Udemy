eabe6582c4ad8e8ecd2140e1b327764029041d2c
Emiliano Guttlein <emiliano.guttlein@globallogic.com>
2023-03-22 11:09:20 -0300

    Testing

--- App.js
+++ App.js
@@ -7,6 +7,9 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    <Text>
+      
+    </Text>
   );
 }
 
