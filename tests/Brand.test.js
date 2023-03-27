7e3c8bfec340106a98261b26e0d0f0463af05306
Emiliano Guttlein <emiliano.guttlein@globallogic.com>
2023-03-27 14:37:33 -0300

    add some test changes

--- tests/Brand.test.js
+++ tests/Brand.test.js
@@ -4,7 +4,7 @@ import Brand from '../components/Brand.js';
 describe('Brand component renders correctly', () => {
   it('renders the brand title', () => {
     const {getByText} = render(<Brand />);
-    const titleElement = getByText('HumanDD');
+    const titleElement = getByText('HumanDB');
     expect(titleElement).toBeDefined();
   });
 });
